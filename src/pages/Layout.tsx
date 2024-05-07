a5b552ec2ec446e0047c05001cdd2d83a7c52f61
mykyta07 <mykytanyk@gmail.com>
2024-05-07 21:28:45 +0300

    added navbar

---
+++ src/pages/Layout.tsx
@@ -0,0 +1,25 @@
+import { Link } from 'react-router-dom';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+
+function Layout() {
+return (
+    <>
+        <AppBar position="static" sx={{ width: '100%' }} >
+            <Toolbar>
+                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                    My App
+                </Typography>
+                <Button color="inherit" component={Link} to="/">Home</Button>
+                <Button color="inherit" component={Link} to="/home">Home Page</Button>
+                <Button color="inherit" component={Link} to="/rent-car-form">Rent Car</Button>
+                <Button color="inherit" component={Link} to="/adding-car-form">Add Car</Button>
+            </Toolbar>
+        </AppBar>
+    </>
+);
+}
+
+export default Layout;
