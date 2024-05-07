99147717d1ce0677c9b4bc290f19206b4bdb4158
mykyta07 <mykytanyk@gmail.com>
2024-05-07 21:24:49 +0300

    added import of pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Car  from "./pages/Mykyta/Car";
+import Layout from "./pages/Layout";
+import Rent from "./pages/Rent";
+import Home from "./pages/Home";
+
+import './App.css';
 
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
+          <Route path="/" element={<Layout />} />
+          <Route path='home' element={<Home />} />
           <Route path="rent-car-form" element={<Rent />} />
           <Route path="adding-car-form" element={<Car />} />
-        </Route>
       </Routes>
     </BrowserRouter>
   );
