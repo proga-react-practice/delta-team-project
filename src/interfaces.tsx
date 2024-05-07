a96dbb69fdf39ee7b526152a7943379c527f1036
mykyta07 <mykytanyk@gmail.com>
2024-05-07 21:27:28 +0300

    added interface FormData for car adding form

--- src/interfaces.tsx
+++ src/interfaces.tsx
@@ -30,3 +30,17 @@ export interface FieldErrors {
     email: boolean;
     placeOfIssue: boolean;
 }
+
+export interface FormData {
+    brand: string;
+    model: string;
+    year: number | string;
+    body_type: string;
+    mileage_km: number | string;
+    gearbox: string;
+    fuel: string;
+    price_per_day: number  | string;
+    horse_power: number  | string ;
+    engine_capacity: number  | string;
+    purpose: string;
+  }
