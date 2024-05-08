b72f6ed414931c4fd7bc03564066fa7a673ca395
mykyta07 <mykytanyk@gmail.com>
2024-05-08 22:28:27 +0300

    fixed interface import

--- src/pages/Mykyta/componets/Form/Form.tsx
+++ src/pages/Mykyta/componets/Form/Form.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { SelectChangeEvent } from "@mui/material";
-import { FormData} from "../../interfaces/interfaces";
+import { FormData} from "../../../../interfaces";
 import {
   TextField,
   FormControl,
