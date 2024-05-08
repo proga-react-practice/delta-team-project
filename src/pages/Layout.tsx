df8306f66b4a658c63347dfed7c9c69e9d632e64
mykyta07 <mykytanyk@gmail.com>
2024-05-08 17:29:00 +0300

    added Outlet in Layout

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
@@ -18,6 +18,7 @@ return (
                 <Button color="inherit" component={Link} to="/adding-car-form">Add Car</Button>
             </Toolbar>
         </AppBar>
+        <Outlet />
     </>
 );
 }
