de7652aad1b2a6983676dead60f5440df7e47089
mykyta07 <mykytanyk@gmail.com>
2024-05-08 18:22:01 +0300

    removed button "home page" from  navbar

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -9,13 +9,12 @@ return (
                 <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                     My App
                 </Typography>
-                <Button color="inherit" component={Link} to="/">Main pages</Button>
-                <Button color="inherit" component={Link} to="/home">Home Page</Button>
+                <Button color="inherit" component={Link} to="/">Main page</Button>
                 <Button color="inherit" component={Link} to="/rent-car-form">Rent Car</Button>
                 <Button color="inherit" component={Link} to="/adding-car-form">Add Car</Button>
             </Toolbar>
         </AppBar>
-        <Outlet />
+        <Outlet/>
     </>
 );
 }
