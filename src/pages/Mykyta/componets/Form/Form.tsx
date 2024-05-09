ba4526b2690d5c7f27b70db362d6b6e8644f246c
mykyta07 <mykytanyk@gmail.com>
2024-05-09 14:43:48 +0300

    added react-hook-form for Car Form

--- src/pages/Mykyta/componets/Form/Form.tsx
+++ src/pages/Mykyta/componets/Form/Form.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { SelectChangeEvent } from "@mui/material";
 import { FormData} from "../../../../interfaces";
+import { useForm, Controller} from "react-hook-form"
 import {
   TextField,
   FormControl,
@@ -50,168 +50,73 @@ const radioOptionsGearbox = [
   { value: 'Automatic', label: 'Automatic' },
 ];
 
-function Form({ onSubmit }: FormProps) {
-  const [formData, setFormData] = React.useState<FormData>(initialFormData);
-
-  const [errors, setErrors] = React.useState<Partial<FormData>>({});
-
-  function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
-    const { name, value } = event.target;
-    setFormData({ ...formData, [name]: value });
-    setErrors({ ...errors, [name]: "" });
-  }
-
-  function handleSelectChange(event: SelectChangeEvent<string>) {
-    const { name, value } = event.target;
-    setFormData({ ...formData, [name]: value });
-    setErrors({ ...errors, [name]: "" });
-  }
-
-  function validateForm() {
-    const newErrors: Partial<FormData> = {};
-    let isValid = true;
-
-    const mileage = parseInt(formData.mileage_km as string, 10);
-
-    if (!mileage || mileage < 1 || mileage > 1000 || formData.mileage_km.toString().startsWith('0')) {
-      newErrors.mileage_km = !mileage ? "Mileage is required" : 
-                             formData.mileage_km.toString().startsWith('0') ? "Mileage should not start with '0'" : 
-                             "Mileage should be between 1 and 1000";
-      isValid = false;
-    }
-
-    if (!formData.engine_capacity || parseInt(formData.engine_capacity.toString(), 10) < 1 || parseInt(formData.engine_capacity.toString(), 10) > 1000 || formData.engine_capacity.toString().startsWith('0')) {
-      newErrors.engine_capacity = !formData.engine_capacity ? "Engine capacity is required" : 
-                                  formData.engine_capacity.toString().startsWith('0') ? "Engine capacity should not start with '0'" : 
-                                  "Engine capacity should be between 1 and 1000";
-      isValid = false;
-    }
-
-    if (!formData.price_per_day || +formData.price_per_day < 1 || formData.price_per_day.toString().startsWith('0')) {
-      newErrors.price_per_day = !formData.price_per_day ? "Price per day is required" : 
-                                +formData.price_per_day < 1 ? "Price per day should be greater than 1" : 
-                                "Price per day should not start with '0'";
-      isValid = false;
-    }
-
-    if (!formData.horse_power || +formData.horse_power < 1 || formData.horse_power.toString().startsWith('0')) {
-      newErrors.horse_power = !formData.horse_power ? "Horse power is required" : 
-                              +formData.horse_power < 1 ? "Horse power should be greater than 1" : 
-                              "Horse power should not start with '0'";
-      isValid = false;
-    }
-
-    if (!formData.brand) {
-      newErrors.brand = "Brand is required";
-      isValid = false;
-    }
-
-    if (!formData.model) {
-      newErrors.model = "Model is required";
-      isValid = false;
-    }
-
-    const year = parseInt(formData.year as string, 10); 
-
-    if (!year || year < 2000 || year > 2024) {
-      newErrors.year = !year ? "Year is required" : 
-                       "Year should be between 2000 and 2024";
-      isValid = false;
-    }
-
-    if (!formData.body_type) {
-      newErrors.body_type = "Body type is required";
-      isValid = false;
-    }
-
-    if (!formData.gearbox) {
-      newErrors.gearbox = "Gearbox is required";
-      isValid = false;
-    }
-
-    if (!formData.fuel) {
-      newErrors.fuel = "Fuel is required";
-      isValid = false;
-    }
-
-  if (!formData.purpose) {
-      newErrors.purpose = "Purpose is required";
-      isValid = false;
-    }
-
-    setErrors(newErrors);
-    return isValid;
-  }
-const handleReset = () => {
-  setFormData(initialFormData);
+const BoxFormStyle = {
+  backgroundColor: "secondary.main",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  flexDirection: "column",
+  width: "100%",
+  maxWidth: { md: "500px", sm: "500px", xs: "500px"},
+  minWidth: { md: "300px", sm: "400px", xs: "370px"},
+  padding: "20px",
+  border: "none",
+  borderRadius: "10px",
+  boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.25)",
+  color: "primary.main",
+  opacity: 0, 
+  animation: 'fadeIn 1.5s forwards', 
+  '@keyframes fadeIn': {
+    '0%': {
+      opacity: 0,
+    },
+    '100%': {
+      opacity: 1,
+    },
+  },
 };
 
-  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
-    event.preventDefault();
-    if (validateForm()) {
-      onSubmit(formData);
-      setFormData(initialFormData);
-    }
-  }
-
-  const BoxFormStyle = {
-    backgroundColor: "secondary.main",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    flexDirection: "column",
-    width: "100%",
-    maxWidth: { md: "500px", sm: "500px", xs: "500px"},
-    minWidth: { md: "300px", sm: "400px", xs: "370px"},
-    padding: "20px",
-    border: "none",
-    borderRadius: "10px",
-    boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.25)",
-    color: "primary.main",
-    opacity: 0, 
-    animation: 'fadeIn 1.5s forwards', 
-    '@keyframes fadeIn': {
-      '0%': {
-        opacity: 0,
-      },
-      '100%': {
-        opacity: 1,
-      },
-    },
-  };
-
-  const TextFieldStyle = {
-    width: "90%",
-    padding: "8px",
-    marginBottom: "10px",
-  };
+const TextFieldStyle = {
+  width: "90%",
+  padding: "8px",
+  marginBottom: "10px",
+};
 
 
-  const ButtonStyle = {
-    width: "10%",
-    padding: "10px",
-    marginTop: "10px",
-    border: "none",
-    borderRadius: "5px",
-    marginRight: "10%",
-  };
-  const BoxButton = {
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "space-evenly",
-    alignItems: "center",
-    width: "100%",
-  };
+const ButtonStyle = {
+  width: "10%",
+  padding: "10px",
+  marginTop: "10px",
+  border: "none",
+  borderRadius: "5px",
+  marginRight: "10%",
+};
+const BoxButton = {
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "space-evenly",
+  alignItems: "center",
+  width: "100%",
+};
 
-  const RadioGroupStyle = {
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "space-around",
-    alignItems: "center",
+const RadioGroupStyle = {
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "space-around",
+  alignItems: "center",
+};
+
+const Form: React.FC<FormProps> = ({ onSubmit }) => {
+  const { register, handleSubmit, control, formState: { errors }, reset } = useForm<FormData>({
+    defaultValues: initialFormData,
+  });
+
+  const handleReset = () => {
+    reset(initialFormData);
   };
 
   return (
-    <Box component="form" onSubmit={handleSubmit} sx={BoxFormStyle}>
+    <Box component="form"  onSubmit={handleSubmit(onSubmit)} sx={BoxFormStyle}>
       <Typography
         variant="h5"
         sx={{ textAlign: "center", color: "primary.main", marginBottom: {xs: "10px" } }}
@@ -220,170 +125,155 @@ const handleReset = () => {
       </Typography>
       <TextField
         label="Brand"
-        name="brand"
-        value={formData.brand}
-        onChange={handleInputChange}
+        {...register("brand", {
+          required: "Brand is required",
+          validate: {
+            isFirstLetterUppercase: value =>
+              /^[A-Z]/.test(value) || "The first letter must be uppercase"
+          }
+        })}
         sx={TextFieldStyle}
-        error={!!errors.brand}
-        helperText={errors.brand}
+        error={Boolean(errors.brand)}
+        helperText={errors.brand?.message}
       />
       <TextField
         label="Model"
-        name="model"
-        value={formData.model}
-        onChange={handleInputChange}
+        {...register("model", { required: "Model is required", validate: {
+          isFirstLetterUppercase: value =>
+            /^[A-Z]/.test(value) || "The first letter must be uppercase"
+        }})}
         sx={TextFieldStyle}
-        error={!!errors.model}
-        helperText={errors.model}
+        error={Boolean(errors.model)}
+        helperText={errors.model?.message}
       />
       <TextField
         label="Year"
-        name="year"
         type="number"
-        value={formData.year}
-        onChange={handleInputChange}
+        {...register("year", { required: "Year is required",  min: { value: 1900, message: "Year must be 1900 or later" },
+        max: { value: new Date().getFullYear(), message: "Year must be this year or earlier" } })}
         sx={TextFieldStyle}
-        error={!!errors.year}
-        helperText={errors.year}
+        error={Boolean(errors.year)}
+        helperText={errors.year?.message}
       />
-      <FormControl
-        error={Boolean(errors.body_type)}
-        sx={[
-          TextFieldStyle,
-          errors.body_type
-            ? { '& .MuiOutlinedInput-root': { '& fieldset': { borderColor: 'red' } } }
-            : {}
-        ]}
-      >
-        <InputLabel>Body Type</InputLabel>
-        <Select
-          name="body_type"
-          value={formData.body_type}
-          onChange={handleSelectChange}
-          sx={{ width: "100%" }}
-        >
-          <MenuItem value="">
-            <em>Select...</em>
+      <FormControl   error={Boolean(errors.body_type)}
+  sx={TextFieldStyle} >
+  <InputLabel>Body Type</InputLabel>
+  <Controller
+    name="body_type"
+    control={control}
+    rules={{ required: "Body type is required" }}
+    render={({ field }) => (
+      <Select {...field}>
+        {bodyTypes.map((type) => (
+          <MenuItem key={type} value={type}>
+            {type}
           </MenuItem>
-          {bodyTypes.map((type) => (
-            <MenuItem key={type} value={type}>
-              {type}
-            </MenuItem>
-          ))}
-        </Select>
-        {errors.body_type && <FormHelperText>{errors.body_type}</FormHelperText>}
-      </FormControl>
+        ))}
+      </Select>
+    )}
+  />
+  {errors.body_type && <FormHelperText error>{errors.body_type.message}</FormHelperText>}
+</FormControl>
       <TextField
         label="Mileage"
-        name="mileage_km"
         type="number"
-        value={formData.mileage_km}
-        onChange={handleInputChange}
+        {...register("mileage_km", { required: "Mileage is required",     min: { value: 0, message: "Mileage must be 0 or bigger" },
+        max: { value: 1000, message: "Mileage must be 1000 or fewer" }  })}
         InputProps={{ endAdornment: <InputAdornment position="end">1,000 km</InputAdornment> }}
         sx={TextFieldStyle}
-        error={!!errors.mileage_km}
-        helperText={errors.mileage_km}
+        error={Boolean(errors.mileage_km)}
+        helperText={errors.mileage_km?.message}
 />
       <FormControl component="fieldset"  sx={TextFieldStyle}>
         <FormLabel component="legend">Gearbox</FormLabel>
-        <RadioGroup
+        <Controller
           name="gearbox"
-          value={formData.gearbox}
-          onChange={handleInputChange}
-          sx={[
-            RadioGroupStyle,
-            errors.gearbox
-              ? { '& .MuiOutlinedInput-root': { '& fieldset': { borderColor: 'red' } } }
-              : {}
-          ]}>
-          {radioOptionsGearbox.map((option) => (
-            <RadioFormControlLabel
-              key={option.value}
-              value={option.value}
-              label={option.label}
-            />
-          ))}
-        </RadioGroup>
-        {errors.gearbox && <FormHelperText sx={{ color: 'red' }}>{errors.gearbox}</FormHelperText>}
+          control={control}
+          rules={{ required: "Gearbox is required" }}
+          render={({ field }) => (
+            <RadioGroup {...field} sx={RadioGroupStyle}>
+              {radioOptionsGearbox.map((option) => (
+                <RadioFormControlLabel
+                key={option.value}
+                value={option.value}
+                label={option.label}
+              />
+              ))}
+            </RadioGroup>
+          )}
+        />
+        {errors.gearbox && <FormHelperText error>{errors.gearbox.message}</FormHelperText>}
       </FormControl>
       <FormControl component="fieldset"  sx={TextFieldStyle}>
         <FormLabel component="legend">Fuel</FormLabel>
-        <RadioGroup
+        <Controller
           name="fuel"
-          value={formData.fuel}
-          onChange={handleInputChange}
-          sx={[
-            RadioGroupStyle,
-            errors.fuel
-              ? { '& .MuiOutlinedInput-root': { '& fieldset': { borderColor: 'red' } } }
-              : {}
-          ]}>
-          {radioOptionsFuel.map((option) => (
-            <RadioFormControlLabel
-              key={option.value}
-              value={option.value}
-              label={option.label}
-            />
-          ))}
-        </RadioGroup>
-        {errors.fuel && <FormHelperText sx={{ color: 'red' }}>{errors.fuel}</FormHelperText>}
+          control={control}
+          rules={{ required: "Fuel is required" }}
+          render={({ field }) => (
+            <RadioGroup {...field} sx={RadioGroupStyle}>
+              {radioOptionsFuel.map((option) => (
+                <RadioFormControlLabel
+                key={option.value}
+                value={option.value}
+                label={option.label}
+              />
+              ))}
+            </RadioGroup>
+          )}
+        />
+        {errors.fuel && <FormHelperText error>{errors.fuel.message}</FormHelperText>}
       </FormControl>
       <TextField
         label="Price Per Day"
-        name="price_per_day"
         type="number"
-        value={formData.price_per_day}
-        onChange={handleInputChange}
+        {...register("price_per_day", { required: "Price is required",  min: { value: 1, message: "Price must be 1 or bigger" }})}
         InputProps={{endAdornment: <InputAdornment position="end">$</InputAdornment> }}
         sx={TextFieldStyle}
-        error={!!errors.price_per_day}
-        helperText={errors.price_per_day}
+        error={Boolean(errors.price_per_day)}
+        helperText={errors.price_per_day?.message}
       />
       <TextField
         label="Horse Power"
-        name="horse_power"
         type="number"
-        value={formData.horse_power}
-        onChange={handleInputChange}
+        {...register("horse_power", { required: "Horse power is required",     min: { value: 1, message: "Horse power must be 1 or bigger" },
+        max: { value: 1000, message: "Horse power must be 1000 or fewer" } })}
         InputProps={{endAdornment: <InputAdornment position="end">kW</InputAdornment> }}
         sx={TextFieldStyle}
-        error={!!errors.horse_power}
-  helperText={errors.horse_power}
+        error={Boolean(errors.horse_power)}
+        helperText={errors.horse_power?.message}
       />
       <TextField
         label="Engine Capacity"
-        name="engine_capacity"
         type="number"
-        value={formData.engine_capacity}
-        onChange={handleInputChange}
+        {...register("engine_capacity", { required: "Engine capacity is required", min: { value: 0.1, message: "Engine capacity must be 0.1 or bigger" },
+        max: { value: 10, message: "Engine capacity must be 10 or fewer" }})}
         InputProps={{endAdornment: <InputAdornment position="end">cm3</InputAdornment> }}
-        sx={TextFieldStyle}
-        error={!!errors.engine_capacity}
-        helperText={errors.engine_capacity}
+        sx={TextFieldStyle} 
+        error={Boolean(errors.engine_capacity)}
+        helperText={errors.engine_capacity?.message}  
       />
       <FormControl
-        error={Boolean(errors.purpose)}
-        sx={[TextFieldStyle, errors.purpose ? { '& .MuiOutlinedInput-root': { '& fieldset': { } } } : {}]}
-      >
-        <InputLabel>Purpose</InputLabel>
-        <Select
-          name="purpose"
-          value={formData.purpose}
-          onChange={handleSelectChange}
-          sx={{ width: "100%" }}
-          error={!!errors.purpose}
-        >
-          <MenuItem value="">
-            <em>Select...</em>
+  error={Boolean(errors.purpose)}
+  sx={TextFieldStyle}
+>
+  <InputLabel>Purpose</InputLabel>
+  <Controller
+    name="purpose"
+    control={control}
+    rules={{ required: "Purpose is required" }}
+    render={({ field }) => (
+      <Select {...field}>
+        {purposes.map((type) => (
+          <MenuItem key={type} value={type}>
+            {type}
           </MenuItem>
-          {purposes.map((purpose) => (
-            <MenuItem key={purpose} value={purpose}>
-              {purpose}
-            </MenuItem>
-          ))}
-        </Select>
-        {errors.purpose && <FormHelperText>{errors.purpose}</FormHelperText>}
-      </FormControl>
+        ))}
+      </Select>
+    )}
+  />
+  {errors.purpose && <FormHelperText error>{errors.purpose.message}</FormHelperText>}
+</FormControl>
       <Box sx={BoxButton}>
         <Button
           type="submit"
