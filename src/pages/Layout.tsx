faf4a5166e3ae0c4331ae13162b55084a0f62a6c
Thundem <vovkandrij7@gmail.com>
2024-05-12 01:14:42 +0300

    added style for navbar and buttons

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -2,21 +2,37 @@ import { Link, Outlet } from 'react-router-dom';
 import {AppBar, Toolbar, Typography, Button } from '@mui/material';
 
 function Layout() {
-return (
-    <>
-        <AppBar position="static" sx={{ width: '100%' }} >
-            <Toolbar>
-                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                    My App
-                </Typography>
-                <Button color="inherit" component={Link} to="/">Main page</Button>
-                <Button color="inherit" component={Link} to="/rent-car-form">Rent Car</Button>
-                <Button color="inherit" component={Link} to="/adding-car-form">Add Car</Button>
-            </Toolbar>
-        </AppBar>
-        <Outlet/>
-    </>
-);
-}
+
+    // const theme = useTheme();
+
+    const NavBar = {
+        width: '100%', 
+        backgroundColor: '#c19f7b', 
+        boxShadow: '0 0 7px rgba(0,0,0,0.15)',
+    }
+
+    const Buttons = {
+        '&:hover': {
+            backgroundColor: '#d3c5b6',
+            color: '#5d3f1f',
+        },
+    }
+
+    return (
+        <>
+            <AppBar position="static" sx={NavBar} >
+                <Toolbar>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        Rent Car
+                    </Typography>
+                    <Button sx={Buttons} color="inherit" component={Link} to="/">Main page</Button>
+                    <Button sx={Buttons} color="inherit" component={Link} to="/rent-car-form">Rent Car</Button>
+                    <Button sx={Buttons} color="inherit" component={Link} to="/adding-car-form">Add Car</Button>
+                </Toolbar>
+            </AppBar>
+            <Outlet/>
+        </>
+    );
+    }
 
 export default Layout;
