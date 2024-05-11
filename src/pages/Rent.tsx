bf3a3d0cddd26d759277f20ac601e16055ff0cf3
Thundem <vovkandrij7@gmail.com>
2024-05-11 23:33:15 +0300

    added edit function for editing cards and some fixes

--- src/pages/Rent.tsx
+++ src/pages/Rent.tsx
@@ -1,27 +1,30 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import RentCarForm from './Andrii/forms/RentCarForm';
 import { FormResults } from './Andrii/cards/FormResult';
 import { FormResultsMobile} from './Andrii/cards/FormResultMobile';
-import { RentCar, initialFormState } from '../interfaces';
+import { RentCar } from '../interfaces';
 import { ThemeProvider } from '@mui/material/styles';
 import { useMediaQuery } from '@mui/material';
 import theme from '../theme';
 import Box from '@mui/material/Box';
 
 function Rent() {
-  const [form, setForm] = useState<RentCar>(initialFormState);
   const [submittedForms, setSubmittedForms] = useState<RentCar[]>([]);
   const isMobile = useMediaQuery(theme.breakpoints.down('lg'));
 
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    setSubmittedForms([...submittedForms, form]);
-    setForm(initialFormState);
+  const handleSubmit = (data: RentCar) => {
+    setSubmittedForms([...submittedForms, data]);
   };
 
   const handleDelete = (index: number) => {
     const newSubmittedForms = [...submittedForms];
     newSubmittedForms.splice(index, 1);
+    setSubmittedForms(newSubmittedForms);
+  };
+
+  const handleEdit = (index: number, newData: RentCar) => {
+    const newSubmittedForms = [...submittedForms];
+    newSubmittedForms[index] = newData;
     setSubmittedForms(newSubmittedForms);
   };
 
@@ -33,10 +36,10 @@ function Rent() {
   }
 
   const RentCar = {
-    width: {xs: '100%', md: '25%', lg: '30%'},
+    width: {xs: '100%', md: '50%', lg: '35%'},
     display: 'flex',
     justifyContent: 'center',
-    alignItems: 'center',
+    alignItems: 'start',
   }
 
   const FormResult = {
@@ -51,14 +54,14 @@ function Rent() {
     <ThemeProvider theme={theme}>
       <Box sx={ RentStyle }>
         <Box sx={ RentCar }>
-          <RentCarForm form={form} setForm={setForm} onSubmit={handleSubmit} />
+          <RentCarForm onSubmit={handleSubmit} />
         </Box>
         <Box sx={FormResult}>
           {isMobile ?
             submittedForms.map((form, index) => ( 
-              <FormResultsMobile key={index} form={form} onDelete={() => handleDelete(index)} />
+              <FormResultsMobile key={index} form={form} onDelete={() => handleDelete(index)} onEdit={(newData) => handleEdit(index, newData)} />
             )) :
-            <FormResults forms={submittedForms} onDelete={handleDelete} />
+            <FormResults forms={submittedForms} onDelete={handleDelete} onEdit={handleEdit} />
           }
         </Box>
       </Box>
