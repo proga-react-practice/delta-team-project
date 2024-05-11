7a287cba2cafc7dd039e62cdcee27a4553e7a70b
Thundem <vovkandrij7@gmail.com>
2024-05-11 23:32:17 +0300

    added opportunity edit form

--- src/pages/Andrii/cards/FormResult.tsx
+++ src/pages/Andrii/cards/FormResult.tsx
@@ -1,19 +1,46 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { RentCar } from '../../../interfaces';
 import { StyledTableCell } from '../styledComponents/StyledTableCell'
 import { StyledTableRow } from '../styledComponents/StyledTableRow'
-import { StyledButton } from '../styledComponents/StyledButton'
+import { StyledButtonDelete } from '../styledComponents/StyledButtonDelete'
+import { StyledButtonEdit } from '../styledComponents/StyledButtonEdit'
+import { StyledButtonSave } from '../styledComponents/StyledButtonSave'
+import { StyledTextField } from '../styledComponents/StyledTextField'
 import { createTransform } from '../animations/animation' 
 import { Table, TableBody, TableContainer, TableHead, TableRow, useTheme } from '@mui/material';
 
 interface FormResultsProps {
   forms: RentCar[];
   onDelete: (index: number) => void;
+  onEdit: (index: number, newData: RentCar) => void;
 }
 
-export const FormResults: React.FC<FormResultsProps> = ({ forms, onDelete }) => {
+export const FormResults: React.FC<FormResultsProps> = ({ forms, onDelete, onEdit }) => {
   const theme = useTheme()
   const Transform = createTransform(theme)
+
+  const [editingIndex, setEditingIndex] = useState<number | null>(null);
+  const [editingData, setEditingData] = useState<RentCar | null>(null);
+
+  const handleEditClick = (index: number) => {
+    setEditingIndex(index);
+    setEditingData(forms[index]);
+  };
+
+  const handleSaveClick = () => {
+    if (editingIndex !== null && editingData !== null) {
+      onEdit(editingIndex, editingData);
+    }
+    setEditingIndex(null);
+    setEditingData(null);
+  };
+
+  const handleInputChange = (prop: keyof RentCar) => (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEditingData(prev => ({
+      ...prev,
+      [prop]: event.target.value || '',
+    }) as RentCar);
+  };
 
   const FormResultStyle = {
     ...Transform,
@@ -46,21 +73,80 @@ export const FormResults: React.FC<FormResultsProps> = ({ forms, onDelete }) => 
           </TableRow>
         </TableHead>
         <TableBody>
-          {forms.map((form, index) => (
-            <StyledTableRow key={index}>
-              <StyledTableCell>{form.firstName}</StyledTableCell>
-              <StyledTableCell>{form.lastName}</StyledTableCell>
-              <StyledTableCell>{form.phoneNumber}</StyledTableCell>
-              <StyledTableCell>{form.email}</StyledTableCell>
-              <StyledTableCell>{form.placeOfIssue}</StyledTableCell>
-              <StyledTableCell>{form.startRentDate.format('YYYY-MM-DD HH:mm')}</StyledTableCell>
-              <StyledTableCell>{form.finishRentDate.format('YYYY-MM-DD HH:mm')}</StyledTableCell>
-              <StyledTableCell>{form.comments}</StyledTableCell>
-              <StyledTableCell>
-                <StyledButton onClick={() => onDelete(index)}>Delete</StyledButton>
-              </StyledTableCell>
-            </StyledTableRow>
-          ))}
+              {forms.map((form, index) => (
+                <StyledTableRow key={index}>
+                  {index === editingIndex ? (
+                    <React.Fragment>
+                      <StyledTableCell>
+                        <StyledTextField
+                          value={editingData?.firstName}
+                          onChange={handleInputChange('firstName')}
+                        />
+                      </StyledTableCell>
+                      <StyledTableCell>
+                      <StyledTextField
+                          value={editingData?.lastName || ''}
+                          onChange={handleInputChange('lastName')}
+                        />
+                      </StyledTableCell>
+                      <StyledTableCell>
+                        <StyledTextField
+                          value={editingData?.phoneNumber}
+                          onChange={handleInputChange('phoneNumber')}
+                        />
+                      </StyledTableCell>
+                      <StyledTableCell>
+                        <StyledTextField
+                          value={editingData?.email}
+                          onChange={handleInputChange('email')}
+                        />
+                      </StyledTableCell>
+                      <StyledTableCell>
+                        <StyledTextField
+                          value={editingData?.placeOfIssue}
+                          onChange={handleInputChange('placeOfIssue')}
+                        />
+                      </StyledTableCell>
+                      <StyledTableCell>
+                        <StyledTextField
+                          value={editingData?.startRentDate.format('YYYY-MM-DD HH:mm')}
+                          onChange={handleInputChange('startRentDate')}
+                        />
+                      </StyledTableCell>
+                      <StyledTableCell>
+                        <StyledTextField
+                          value={editingData?.finishRentDate.format('YYYY-MM-DD HH:mm')}
+                          onChange={handleInputChange('finishRentDate')}
+                        />
+                      </StyledTableCell>
+                      <StyledTableCell>
+                        <StyledTextField
+                          value={editingData?.comments}
+                          onChange={handleInputChange('comments')}
+                        />
+                      </StyledTableCell>
+                      <StyledTableCell>
+                        <StyledButtonSave onClick={handleSaveClick}>Save</StyledButtonSave>
+                      </StyledTableCell>                    
+                    </React.Fragment>
+                  ) : (
+                    <React.Fragment>
+                      <StyledTableCell>{form.firstName}</StyledTableCell>
+                      <StyledTableCell>{form.lastName}</StyledTableCell>
+                      <StyledTableCell>{form.phoneNumber}</StyledTableCell>
+                      <StyledTableCell>{form.email}</StyledTableCell>
+                      <StyledTableCell>{form.placeOfIssue}</StyledTableCell>
+                      <StyledTableCell>{form.startRentDate.format('YYYY-MM-DD HH:mm')}</StyledTableCell>
+                      <StyledTableCell>{form.finishRentDate.format('YYYY-MM-DD HH:mm')}</StyledTableCell>
+                      <StyledTableCell>{form.comments}</StyledTableCell>
+                      <StyledTableCell>
+                        <StyledButtonDelete onClick={() => onDelete(index)}>Delete</StyledButtonDelete>
+                        <StyledButtonEdit onClick={() => handleEditClick(index)}>Edit</StyledButtonEdit>
+                      </StyledTableCell>
+                    </React.Fragment>
+                  )}
+                </StyledTableRow>
+              ))}
         </TableBody>
       </Table>
     </TableContainer>
--- src/pages/Andrii/cards/FormResultMobile.tsx
+++ src/pages/Andrii/cards/FormResultMobile.tsx
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
+import dayjs from 'dayjs';
 import { RentCar } from '../../../interfaces';
 import { StyledTableCellMobile } from '../styledComponents/StyledTableCellMobile'
 import { StyledTableRow } from '../styledComponents/StyledTableRow'
-import { StyledButton } from '../styledComponents/StyledButton'
+import { StyledButtonDelete } from '../styledComponents/StyledButtonDelete'
+import { StyledButtonEdit } from '../styledComponents/StyledButtonEdit'
+import { StyledButtonSave } from '../styledComponents/StyledButtonSave'
+import { StyledTextField } from '../styledComponents/StyledTextField'
 import { createTransform } from '../animations/animation' 
 import { Table, TableBody, TableContainer, useTheme } from '@mui/material';
 
 interface FormResultsMobileProps {
   form: RentCar;
   onDelete: () => void;
+  onEdit: (newData: RentCar) => void;
 }
 
-export const FormResultsMobile: React.FC<FormResultsMobileProps> = ({ form, onDelete }) => {
+export const FormResultsMobile: React.FC<FormResultsMobileProps> = ({ form, onDelete, onEdit }) => {
     const theme = useTheme()
     const Transform = createTransform(theme)
+    const [editingData, setEditingData] = useState<RentCar | null>(null);
+
+    const handleEditClick = () => {
+      setEditingData(form);
+    };
+  
+    const handleSaveClick = () => {
+      if (editingData !== null) {
+        onEdit(editingData);
+      }
+      setEditingData(null);
+    };
   
     const MobileStyle = {
       display: 'flex',
@@ -32,39 +49,94 @@ export const FormResultsMobile: React.FC<FormResultsMobileProps> = ({ form, onDe
               <TableBody sx={Transform}>
                   <StyledTableRow>
                       <StyledTableCellMobile>First Name</StyledTableCellMobile>
-                      <StyledTableCellMobile>{form.firstName}</StyledTableCellMobile>
+                      <StyledTableCellMobile>
+                        {editingData ? 
+                            <StyledTextField value={editingData.firstName} onChange={(e) => setEditingData({...editingData, firstName: e.target.value})} /> 
+                            : 
+                            form.firstName
+                        }
+                        </StyledTableCellMobile>
                   </StyledTableRow>
                   <StyledTableRow>
                       <StyledTableCellMobile>Last Name</StyledTableCellMobile>
-                      <StyledTableCellMobile>{form.lastName}</StyledTableCellMobile>
+                      <StyledTableCellMobile>
+                        {editingData ? 
+                            <StyledTextField value={editingData.lastName} onChange={(e) => setEditingData({...editingData, lastName: e.target.value})} /> 
+                            : 
+                            form.lastName
+                        }
+                        </StyledTableCellMobile>
                   </StyledTableRow>
                   <StyledTableRow>
                       <StyledTableCellMobile>Phone Number</StyledTableCellMobile>
-                      <StyledTableCellMobile>{form.phoneNumber}</StyledTableCellMobile>
+                      <StyledTableCellMobile>
+                        {editingData ? 
+                            <StyledTextField value={editingData.phoneNumber} onChange={(e) => setEditingData({...editingData, phoneNumber: e.target.value})} /> 
+                            : 
+                            form.phoneNumber
+                        }
+                        </StyledTableCellMobile>
                   </StyledTableRow>
                   <StyledTableRow>
                       <StyledTableCellMobile>Email</StyledTableCellMobile>
-                      <StyledTableCellMobile>{form.email}</StyledTableCellMobile>
+                      <StyledTableCellMobile>
+                        {editingData ? 
+                            <StyledTextField value={editingData.email} onChange={(e) => setEditingData({...editingData, email: e.target.value})} /> 
+                            : 
+                            form.email
+                        }
+                        </StyledTableCellMobile>
                   </StyledTableRow>
                   <StyledTableRow>
                       <StyledTableCellMobile>Place Of Issue</StyledTableCellMobile>
-                      <StyledTableCellMobile>{form.placeOfIssue}</StyledTableCellMobile>
+                      <StyledTableCellMobile>
+                        {editingData ? 
+                            <StyledTextField value={editingData.placeOfIssue} onChange={(e) => setEditingData({...editingData, placeOfIssue: e.target.value})} /> 
+                            : 
+                            form.placeOfIssue
+                        }
+                        </StyledTableCellMobile>
                   </StyledTableRow>
                   <StyledTableRow>
                       <StyledTableCellMobile>Start Rent Date</StyledTableCellMobile>
-                      <StyledTableCellMobile>{form.startRentDate.format('YYYY-MM-DD HH:mm')}</StyledTableCellMobile>
+                      <StyledTableCellMobile>
+                        {editingData ? 
+                            <StyledTextField value={editingData.startRentDate.format('YYYY-MM-DD HH:mm')} onChange={(e) => setEditingData({...editingData, startRentDate: dayjs(e.target.value)})} /> 
+                            : 
+                            form.startRentDate.format('YYYY-MM-DD HH:mm')
+                        }
+                        </StyledTableCellMobile>
                   </StyledTableRow>
                   <StyledTableRow>
-                      <StyledTableCellMobile>End Rent Date</StyledTableCellMobile>
-                      <StyledTableCellMobile>{form.finishRentDate.format('YYYY-MM-DD HH:mm')}</StyledTableCellMobile>
+                      <StyledTableCellMobile>Finish Rent Date</StyledTableCellMobile>
+                      <StyledTableCellMobile>
+                        {editingData ? 
+                            <StyledTextField value={editingData.finishRentDate.format('YYYY-MM-DD HH:mm')} onChange={(e) => setEditingData({...editingData, finishRentDate: dayjs(e.target.value)})} /> 
+                            : 
+                            form.finishRentDate.format('YYYY-MM-DD HH:mm')
+                        }
+                        </StyledTableCellMobile>
                   </StyledTableRow>
                   <StyledTableRow>
                       <StyledTableCellMobile>Comments</StyledTableCellMobile>
-                      <StyledTableCellMobile>{form.comments}</StyledTableCellMobile>
+                      <StyledTableCellMobile>
+                        {editingData ? 
+                            <StyledTextField value={editingData.comments} onChange={(e) => setEditingData({...editingData, comments: e.target.value})} /> 
+                            : 
+                            form.comments
+                        }
+                        </StyledTableCellMobile>
                   </StyledTableRow>
-                  <StyledTableRow sx={{ backgroundColor: 'f5f5f5', justifyContent: 'flex-end',}}>
-                      <StyledTableCellMobile colSpan={2}>
-                          <StyledButton onClick={onDelete}>Delete</StyledButton>
+                  <StyledTableRow>
+                      <StyledTableCellMobile>
+                          <StyledButtonDelete onClick={onDelete}>Delete</StyledButtonDelete>
+                      </StyledTableCellMobile>
+                      <StyledTableCellMobile>
+                            {editingData ? 
+                                <StyledButtonSave onClick={handleSaveClick}>Save</StyledButtonSave>
+                                : 
+                                <StyledButtonEdit onClick={handleEditClick}>Edit</StyledButtonEdit>
+                            }
                       </StyledTableCellMobile>
                   </StyledTableRow>
               </TableBody>
