6dc57c49423efe25b9df332cca10465f29fe7a42
mykyta07 <mykytanyk@gmail.com>
2024-05-12 22:47:09 +0300

    provided theme and added info about project to home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,34 @@
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "../theme";
+import { CssBaseline, Typography, Box } from "@mui/material";
+
+const ContainerHomeStyle = {
+  display: "flex",
+  width: "100%",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+};
+
+const TypographyStyle = {
+  marginBottom: '1%', 
+};
+
 function Home() {
   return (
-    <h1>Hello Home!</h1>
+    <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <Box sx={ContainerHomeStyle}>
+        <Typography variant="h2"  sx={TypographyStyle}>About our project</Typography>
+        <Typography variant="h6"  sx={TypographyStyle} >
+          Our team is working on a car rental web project that aims to simplify the process of renting cars. 
+        </Typography>
+        <Typography variant="h2"  sx={TypographyStyle}>Our Team</Typography>
+        <Typography variant="h6"  sx={TypographyStyle}>
+         Mykyta made the form for adding a car. Andrii made the form for renting a car. To connect these two forms, we used the react-router-dom library.
+        </Typography>
+    </Box>
+    </ThemeProvider>
   );
 }
 
