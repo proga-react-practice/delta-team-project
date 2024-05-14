67f1e9b226d230bf6b4f89b4fa3aed4884a7eff1
mykyta07 <mykytanyk@gmail.com>
2024-05-14 17:30:13 +0300

    added error path

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Car  from "./pages/Mykyta/Car";
 import Layout from "./pages/Layout";
 import Rent from "./pages/Rent";
 import Home from "./pages/Home";
+import Error from "./pages/Error";
 
 import './App.css';
 
@@ -15,6 +16,7 @@ function App() {
           <Route index element={<Home />} />
           <Route path="rent-car-form" element={<Rent />} />
           <Route path="adding-car-form" element={<Car />} />
+          <Route path="*" element={<Error/>} />
         </Route>
       </Routes>
     </BrowserRouter>
