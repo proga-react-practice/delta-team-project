9f3aff80ee87bafec81f837137931e1b9500de05
mykyta07 <mykytanyk@gmail.com>
2024-05-18 01:00:55 +0300

    added basic dark mode theme

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,6 +1,6 @@
 import { createTheme } from "@mui/material/styles";
 
-const theme = createTheme({
+export const theme = createTheme({
   palette: {
     primary: {
       main: "#000000",
@@ -35,4 +35,39 @@ const theme = createTheme({
   },
 });
 
-export default theme;
+export const darkTheme = createTheme({
+  palette: {
+    primary: {
+      main: "#000000",
+      dark: "#388023",
+    },
+    secondary: {
+      main: "#eeeeee",
+      dark: '#484542',
+      light: '#ffffff',
+    },
+    background: {
+      default: "#000000",
+    },
+    error: {
+      main: '#c84238',
+      light: '#dc3545',
+      dark: '#9a000f'
+    },
+    success: {
+      main: '#28a745',
+      light: '#34d058',
+      dark: '#22863a'
+    },
+    info: {
+      main: '#17a2b8',
+      light: '#007bff',
+      dark: '#003d7f'
+    },
+  },
+  typography: {
+    fontFamily: 'Goudy Bookletter 1911',
+  },
+});
+
+
