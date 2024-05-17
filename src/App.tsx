180bb813cf4208beedc6c82bf39a7f10529dddca
mykyta07 <mykytanyk@gmail.com>
2024-05-18 01:01:40 +0300

    added function to change theme

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,30 @@ import Layout from "./pages/Layout";
 import Rent from "./pages/Rent";
 import Home from "./pages/Home";
 import Error from "./pages/Error";
-import { CssBaseline, ThemeProvider } from "@mui/material";
-import theme from "./theme";
+import { CssBaseline, ThemeProvider} from "@mui/material";
+import { theme, darkTheme } from "./theme";
+
+import { useState } from 'react';
 
 import './App.css';
 
+
+
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+const toggleDarkMode = () => {
+  setDarkMode(!darkMode);
+};
+
+const currentTheme = darkMode ? darkTheme : theme;
 
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={currentTheme}>
     <CssBaseline/>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />} >
+        <Route path="/" element={<Layout darkMode={darkMode} toggleDarkMode={toggleDarkMode} />} >
           <Route index element={<Home />} />
           <Route path="rent-car-form" element={<Rent />} />
           <Route path="adding-car-form" element={<Car />} />
