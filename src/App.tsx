8e59d82ff8f3b575efdec7bdabb0342f6f921b17
mykyta07 <mykytanyk@gmail.com>
2024-05-18 23:54:03 +0300

    added Context provider

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Home from "./pages/Home";
 import Error from "./pages/Error";
 import { CssBaseline, ThemeProvider} from "@mui/material";
 import { theme, darkTheme } from "./theme";
+import { FormProvider } from './Context';
 import { useState } from 'react';
 import './App.css';
 
@@ -19,19 +20,21 @@ function App() {
   const currentTheme = darkMode ? darkTheme : theme;
 
   return (
-    <ThemeProvider theme={currentTheme}>
-    <CssBaseline/>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout darkMode={darkMode} toggleDarkMode={toggleDarkMode} />} >
-          <Route index element={<Home />} />
-          <Route path="rent-car-form" element={<Rent />} />
-          <Route path="adding-car-form" element={<Car />} />
-          <Route path="*" element={<Error/>} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-    </ThemeProvider>
+    <FormProvider>
+      <ThemeProvider theme={currentTheme}>
+      <CssBaseline/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout darkMode={darkMode} toggleDarkMode={toggleDarkMode} />} >
+            <Route index element={<Home />} />
+            <Route path="rent-car-form" element={<Rent />} />
+            <Route path="adding-car-form" element={<Car />} />
+            <Route path="*" element={<Error/>} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+      </ThemeProvider>
+    </FormProvider>
   );
 }
 
