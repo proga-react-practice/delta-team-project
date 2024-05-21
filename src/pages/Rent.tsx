9c0aa56b2b888b2ecd9751d6672682ba0efb1512
Thundem <vovkandrij7@gmail.com>
2024-05-21 14:48:56 +0300

    added useFieldArray to cards

--- src/pages/Rent.tsx
+++ src/pages/Rent.tsx
@@ -1,30 +1,51 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import RentCarForm from './Andrii/forms/RentCarForm';
 import { FormResults } from './Andrii/cards/FormResult';
-import { FormResultsMobile} from './Andrii/cards/FormResultMobile';
-import { RentCar } from '../interfaces';
-import { useMediaQuery} from '@mui/material';
+import { FormResultsMobile } from './Andrii/cards/FormResultMobile';
+import { RentCar, OrderGroup } from '../interfaces';
+import { useMediaQuery } from '@mui/material';
 import { theme } from '../theme';
 import Box from '@mui/material/Box';
+import { useRentCarContext } from './Andrii/RentCarContext';
+import { useFieldArray, useForm } from "react-hook-form";
 
-function Rent() {
-  const [submittedForms, setSubmittedForms] = useState<RentCar[]>([]);
+const Rent: React.FC = () => {
   const isMobile = useMediaQuery(theme.breakpoints.down('lg'));
+  const { orderGroup, addOrder, updateOrder, removeOrder } = useRentCarContext();
+  const { control, setValue } = useForm<OrderGroup>();
+  const [editingCardIndex, setEditingCardIndex] = useState<number | null>(null);
+  const { fields, append, update, remove } = useFieldArray({
+    control,
+    name: "orders",
+  });
+
+  useEffect(() => {
+    setValue('orders', orderGroup.orders);
+  }, [orderGroup.orders, setValue]);
+
+  const handleEdit = (index: number) => {
+    console.log('Edit clicked');
+    console.log(index);
+    setEditingCardIndex(index);
+  };
 
   const handleSubmit = (data: RentCar) => {
-    setSubmittedForms([...submittedForms, data]);
+    console.log('Submit clicked');
+    console.log(data);
+    append(data)
+    addOrder(data);
   };
 
   const handleDelete = (index: number) => {
-    const newSubmittedForms = [...submittedForms];
-    newSubmittedForms.splice(index, 1);
-    setSubmittedForms(newSubmittedForms);
+    remove(index);
+    removeOrder(index);
   };
 
-  const handleEdit = (index: number, newData: RentCar) => {
-    const newSubmittedForms = [...submittedForms];
-    newSubmittedForms[index] = newData;
-    setSubmittedForms(newSubmittedForms);
+  const handleSave = (index: number, data: RentCar) => {
+    console.log('clicked');
+    update(index, data);
+    updateOrder(index, data);
+    setEditingCardIndex(null);
   };
 
   const RentStyle = {
@@ -35,35 +56,50 @@ function Rent() {
   }
 
   const RentCar = {
-    width: {xs: '100%', md: '50%', lg: '35%'},
+    width: { xs: '100%', md: '50%', lg: '35%' },
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'start',
-  }
+  };
 
   const FormResult = {
-    width: {xs: '100%', md: '67%', lg: '60%'},
+    width: { xs: '100%', md: '67%', lg: '60%' },
     display: 'flex',
-    flexDirection: {lg: 'row', xs: 'column'},
+    flexDirection: { lg: 'row', xs: 'column' },
     justifyContent: 'space-around',
-    alignItems: {lg: 'start', md: 'center', xs: 'center'},
-  }
+    alignItems: { lg: 'start', md: 'center', xs: 'center' },
+  };
 
   return (
-      <Box sx={ RentStyle }>
-        <Box sx={ RentCar }>
-          <RentCarForm onSubmit={handleSubmit} />
-        </Box>
-        <Box sx={FormResult}>
-          {isMobile ?
-            submittedForms.map((form, index) => ( 
-              <FormResultsMobile key={index} form={form} onDelete={() => handleDelete(index)} onEdit={(newData) => handleEdit(index, newData)} />
-            )) :
-            <FormResults forms={submittedForms} onDelete={handleDelete} onEdit={handleEdit} />
-          }
-        </Box>
+    <Box sx={RentStyle}>
+      <Box sx={RentCar}>
+        <RentCarForm onSubmit={handleSubmit} />
       </Box>
+      <Box sx={FormResult}>
+      {fields.map((form, index) => (
+        isMobile ? 
+          <FormResultsMobile
+            key={index}
+            form={form}
+            onDelete={handleDelete}
+            onSave={(data) => handleSave(index, data)}
+            index={index}
+            onEdit={handleEdit}
+            isEditing={index === editingCardIndex}
+          /> :
+          <FormResults
+            key={index}
+            form={form}
+            onSave={(form) => handleSave(index, form)}
+            onDelete={handleDelete}
+            onEdit={handleEdit}
+            index={index} 
+            isEditing={index === editingCardIndex}
+          />
+      ))}
+      </Box>
+    </Box>
   );
-}
+};
 
 export default Rent;
