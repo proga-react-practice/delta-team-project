53dd0dfa42b81ed97954adc7ef877469cc5a6b39
Thundem <vovkandrij7@gmail.com>
2024-05-21 14:50:50 +0300

    updated cards for using useFieldArray

--- src/pages/Andrii/cards/FormResultMobile.tsx
+++ src/pages/Andrii/cards/FormResultMobile.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import dayjs from 'dayjs';
 import { RentCar } from '../../../interfaces';
 import { textPattern, numberPattern, emailPattern, format } from '../../../pattern';
@@ -17,26 +17,29 @@ import { Table, TableBody, TableContainer } from '@mui/material';
 
 interface FormResultsMobileProps {
   form: RentCar;
-  onDelete: () => void;
-  onEdit: (newData: RentCar) => void;
+  onDelete: (index: number) => void;
+  index: number;
+  onSave: (data: RentCar) => void;
+  isEditing: boolean;
+  onEdit: (index: number) => void;
 }
 
-export const FormResultsMobile: React.FC<FormResultsMobileProps> = ({ form, onDelete, onEdit }) => {
-  const Transform = createTransform()
-  const [editingData, setEditingData] = useState<RentCar | null>(null);
-
-  const { handleSubmit, control } = useForm<RentCar>();
-
-  const handleEditClick = () => {
-    setEditingData(form);
-  };
-
-  const handleSaveClick = handleSubmit((data: RentCar) => {
-    if (data !== null) {
-      onEdit(data);
-    }
-    setEditingData(null);
+export const FormResultsMobile: React.FC<FormResultsMobileProps> = ({ form, onDelete, index, onSave, onEdit, isEditing }) => {
+  
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm<RentCar>({
+    defaultValues: form,
+    mode: "onChange",
   });
+  const Transform = createTransform()
+
+  const onSubmit = (data: RentCar) => {
+    isEditing ? onSave(data) : onEdit(index);
+  };
 
   const MobileStyle = {
     display: 'flex',
@@ -50,32 +53,24 @@ export const FormResultsMobile: React.FC<FormResultsMobileProps> = ({ form, onDe
   }  
 
   return (
-    <TableContainer sx={ MobileStyle }>
+    <TableContainer sx={ MobileStyle } component={'form'} onSubmit={handleSubmit(onSubmit)}>
         <Table aria-label="customize table">
             <TableBody sx={Transform}>
                 <StyledTableRow>
                     <StyledTableCellMobile>First Name</StyledTableCellMobile>
                     <StyledTableCellMobile>
-                      {editingData ? 
-                          <Controller
-                            name="firstName"
-                            control={control}
-                            defaultValue={editingData.firstName}
-                            rules={{
-                              required: 'First Name is required',
+                      {isEditing ? 
+                        <StyledTextField 
+                            {...register('firstName', { 
+                              required: 'First name is required', 
                               pattern: {
-                                value: textPattern,
-                                message: 'First Name must start with a capital letter and cannot contain numbers or special characters'
-                              }
-                            }}
-                            render={({ field: { onChange, value }, fieldState: { error } }) => (
-                              <StyledTextField 
-                                value={value} 
-                                onChange={onChange} 
-                                error={!!error}
-                                helperText={error ? error.message : null}
-                              />
-                            )}
+                                  value: textPattern, 
+                                  message: 'First name must start with a capital letter and cannot contain numbers or special characters'
+                              } 
+                          })}
+                          name="firstName"
+                          error={Boolean(errors.firstName)}
+                          helperText={errors.firstName?.message}
                         />
                           : 
                           form.firstName
@@ -85,128 +80,96 @@ export const FormResultsMobile: React.FC<FormResultsMobileProps> = ({ form, onDe
                 <StyledTableRow>
                     <StyledTableCellMobile>Last Name</StyledTableCellMobile>
                     <StyledTableCellMobile>
-                      {editingData ? 
-                        <Controller
+                      {isEditing ? 
+                        <StyledTextField 
+                          {...register('lastName', { 
+                              required: 'Last name is required', 
+                              pattern: {
+                                  value: textPattern, 
+                                  message: 'Last name must start with a capital letter and cannot contain numbers or special characters'
+                              } 
+                          })}
                           name="lastName"
-                          control={control}
-                          defaultValue={editingData.lastName}
-                          rules={{
-                            required: 'This field is required',
-                            pattern: {
-                              value: textPattern,
-                              message: 'Last Name must start with a capital letter and cannot contain numbers or special characters'
-                            }
-                          }}
-                          render={({ field: { onChange, value }, fieldState: { error } }) => (
-                            <StyledTextField 
-                              value={value} 
-                              onChange={onChange} 
-                              error={!!error}
-                              helperText={error ? error.message : null}
-                            />
-                          )}
+                          error={Boolean(errors.lastName)}
+                          helperText={errors.lastName?.message}
                         />
-                          : 
-                          form.lastName
+                        : 
+                        form.lastName
                       }
                       </StyledTableCellMobile>
                 </StyledTableRow>
                 <StyledTableRow>
                     <StyledTableCellMobile>Phone Number</StyledTableCellMobile>
                     <StyledTableCellMobile>
-                      {editingData ? 
-                          <Controller
-                              name="phoneNumber"
-                              control={control}
-                              defaultValue={editingData.phoneNumber}
-                              rules={{
-                                  required: 'Phone number is required',
-                                  pattern: {
-                                      value: numberPattern,
-                                      message: 'Phone number must be in the format +38(0xx) xxx xxxx and contain only digits.'
-                                  }
-                              }}
-                              render={({ field: { onChange, value }, fieldState: { error } }) => (
-                                  <StyledTextField 
-                                      value={value} 
-                                      onChange={onChange} 
-                                      error={!!error}
-                                      helperText={error ? error.message : null}
-                                  />
-                              )}
-                          />
-                          : 
-                          form.phoneNumber
+                      {isEditing ? 
+                        <StyledTextField 
+                          {...register('phoneNumber', { 
+                              required: 'Phone number is required', 
+                              pattern: {
+                                  value: numberPattern, 
+                                  message: 'Phone number must be in format: +38(0__) ___ ____'
+                              } 
+                          })}
+                          error={Boolean(errors.phoneNumber)}
+                          helperText={errors.phoneNumber?.message}
+                        />
+
+                        : 
+                        form.phoneNumber
                       }
                       </StyledTableCellMobile>
                 </StyledTableRow>
                 <StyledTableRow>
                     <StyledTableCellMobile>Email</StyledTableCellMobile>
                     <StyledTableCellMobile>
-                      {editingData ? 
-                          <Controller
-                              name="email"
-                              control={control}
-                              defaultValue={editingData.email}
-                              rules={{
-                                  required: 'Email is required',
-                                  pattern: {
-                                      value: emailPattern,
-                                      message: 'Email must contain @'
-                                  }
-                              }}
-                              render={({ field: { onChange, value }, fieldState: { error } }) => (
-                                  <StyledTextField 
-                                      value={value} 
-                                      onChange={onChange} 
-                                      error={!!error}
-                                      helperText={error ? error.message : null}
-                                  />
-                              )}
-                          />
-                          : 
-                          form.email
+                      {isEditing ? 
+                        <StyledTextField 
+                          {...register('email', { 
+                              required: 'Email is required', 
+                              pattern: {
+                                  value: emailPattern,
+                                  message: 'Email must contain @'
+                              } 
+                          })}
+                          name='email'
+                          error={Boolean(errors.email)}
+                          helperText={errors.email?.message}
+                        />
+                        : 
+                        form.email
                       }
                       </StyledTableCellMobile>
                 </StyledTableRow>
                 <StyledTableRow>
                     <StyledTableCellMobile>Place Of Issue</StyledTableCellMobile>
                     <StyledTableCellMobile>
-                      {editingData ? 
-                          <Controller
-                              name="placeOfIssue"
-                              control={control}
-                              defaultValue={editingData.placeOfIssue}
-                              rules={{
-                                  required: 'Place of Issue is required',
-                                  pattern: {
-                                      value: textPattern,
-                                      message: 'Place of Issue must start with a capital letter and cannot contain numbers or special characters'
-                                  }
-                              }}
-                              render={({ field: { onChange, value }, fieldState: { error } }) => (
-                                  <StyledTextField 
-                                      value={value} 
-                                      onChange={onChange} 
-                                      error={!!error}
-                                      helperText={error ? error.message : null}
-                                  />
-                              )}
-                          />
-                          : 
-                          form.placeOfIssue
+                      {isEditing ? 
+                        <StyledTextField 
+                          {...register('placeOfIssue', { 
+                            required: 'Place of Issue is required', 
+                            pattern: {
+                                value: textPattern, 
+                                message: 'Place of Issue must start with a capital letter and cannot contain numbers or special characters'
+                            } 
+                          })}
+                          name='placeOfIssue'
+                          error={Boolean(errors.placeOfIssue)}
+                          helperText={errors.placeOfIssue?.message}
+                        />
+                        : 
+                        form.placeOfIssue
                       }
                       </StyledTableCellMobile>
                 </StyledTableRow>
                 <StyledTableRow>
                   <StyledTableCellMobile>Start Rent Date</StyledTableCellMobile>
                   <StyledTableCellMobile>
-                    {editingData ? 
+                    {isEditing ? 
                       <LocalizationProvider dateAdapter={AdapterDayjs}>
                         <Controller 
                           name="startRentDate"
                           control = {control}
-                          defaultValue={editingData.startRentDate}
+                          defaultValue={form.startRentDate}
                           render={ ({field}) => (
                             <DateTimePicker
                               {...field}
@@ -225,17 +188,17 @@ export const FormResultsMobile: React.FC<FormResultsMobileProps> = ({ form, onDe
               <StyledTableRow>
                   <StyledTableCellMobile>Finish Rent Date</StyledTableCellMobile>
                   <StyledTableCellMobile>
-                    {editingData ? 
+                    {isEditing ? 
                         <LocalizationProvider dateAdapter={AdapterDayjs}>
                           <Controller 
                             name="finishRentDate"
                             control = {control}
-                            defaultValue={editingData.finishRentDate}
+                            defaultValue={form.finishRentDate}
                             render={ ({field}) => (
                               <DateTimePicker
                                   {...field}
                                   format={format}
-                                  minDateTime={editingData.finishRentDate ? editingData.startRentDate?.add(5, 'hour') : dayjs()}
+                                  minDateTime={form.finishRentDate ? form.startRentDate?.add(5, 'hour') : dayjs()}
                               />
                             )}
                           />
@@ -248,11 +211,11 @@ export const FormResultsMobile: React.FC<FormResultsMobileProps> = ({ form, onDe
                 <StyledTableRow>
                     <StyledTableCellMobile>Comments</StyledTableCellMobile>
                     <StyledTableCellMobile>
-                      {editingData ? 
+                      {isEditing ? 
                           <Controller
                               name="comments"
                               control={control}
-                              defaultValue={editingData.comments}
+                              defaultValue={form.comments}
                               render={({ field: { onChange, value } }) => (
                                   <StyledTextField 
                                       value={value} 
@@ -271,14 +234,14 @@ export const FormResultsMobile: React.FC<FormResultsMobileProps> = ({ form, onDe
                 </StyledTableRow>
                 <StyledTableRow>
                     <StyledTableCellMobile>
-                        <StyledButtonDelete onClick={onDelete}>Delete</StyledButtonDelete>
+                      {isEditing ? 
+                        <StyledButtonSave type='submit'>Save</StyledButtonSave>
+                        :
+                        <StyledButtonEdit type='submit'>Edit</StyledButtonEdit>
+                      }  
                     </StyledTableCellMobile>
                     <StyledTableCellMobile>
-                          {editingData ? 
-                              <StyledButtonSave onClick={handleSaveClick}>Save</StyledButtonSave>
-                              : 
-                              <StyledButtonEdit onClick={handleEditClick}>Edit</StyledButtonEdit>
-                          }
+                      <StyledButtonDelete variant="contained" onClick={() => onDelete(index)}>Delete</StyledButtonDelete>
                     </StyledTableCellMobile>
                 </StyledTableRow>
             </TableBody>
