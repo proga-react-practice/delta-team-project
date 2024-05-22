8bdb184bc2f25af583079a03daba7d53251eeda0
mykyta07 <mykytanyk@gmail.com>
2024-05-22 19:32:48 +0300

    moved cards to separate page

--- src/pages/Car.tsx
+++ src/pages/Car.tsx
@@ -1,45 +1,29 @@
-import Form from "./Mykyta/componets/Form/Form";
-import Card from "./Mykyta/componets/Card/Card";
+
+import Form from "./Mykyta/Form/Form";
 import { CarGroup, FormData } from "../interfaces";
 import { Box } from "@mui/material";
 import { useFieldArray, useForm } from "react-hook-form";
-import { useState } from "react";
 import { useCarGroupContext } from '../Context';
 import { useEffect } from "react";
 
 function Car() {
   const { control, setValue } = useForm<CarGroup>();
-  const { carGroup , addCar, updateCar, removeCar } = useCarGroupContext();
-  const { fields, append, update, remove } = useFieldArray({
+  const { carGroup , addCar } = useCarGroupContext();
+  const { append,} = useFieldArray({
     control,
     name: "cars",
   });
 
-  const [editingCardIndex, setEditingCardIndex] = useState<number | null>(null);
 
   useEffect(() => {
     setValue('cars', carGroup.cars);
   }, [carGroup.cars, setValue]);
 
-  const handleEditClick = (index: number) => {
-    setEditingCardIndex(index);
-  };
 
   function handleAddNewCar(formData: FormData) {
     append(formData);
     addCar(formData);
   }
-
-  const handleDelete = (index: number) => {
-    remove(index);
-    removeCar(index);
-  };
-
-  const handleSave = (index: number, data: FormData) => {
-    update(index, data);
-    updateCar(index, data);
-    setEditingCardIndex(null);
-  };
 
   const ContainerStyle = {
     display: "flex",
@@ -58,34 +42,15 @@ function Car() {
     alignItems: { sm: "flex-start", xs: "center" },
   };
 
-  const CardLayout = {
-    width: { md: "30%", sm: "100%", xs: "100%" },
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: { md: "flex-start", sm: "center", xs: "center" },
-    alignItems: { sm: "center", xs: "center" },
-  };
-
   return (
       <Box sx={ContainerStyle}>
         <Box sx={FormLayout}>
           <Form onSubmit={handleAddNewCar} />
-        </Box>
-        <Box sx={CardLayout}>
-          {fields.map((data, index) => (
-            <Card
-              key={index}
-              data={data}
-              onDelete={handleDelete}
-              onSave={(data) => handleSave(index, data)}
-              index={index}
-              onEdit={handleEditClick}
-              isEditing={index === editingCardIndex}
-            />
-          ))}
         </Box>
       </Box>
   );
 }
 
 export default Car;
+
+
