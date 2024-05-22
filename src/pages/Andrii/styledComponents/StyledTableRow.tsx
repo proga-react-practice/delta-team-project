5cf392f7b10ac4b0daf40acf797287384025d944
Thundem <vovkandrij7@gmail.com>
2024-05-22 21:25:48 +0300

    added Transform

--- src/pages/Andrii/styledComponents/StyledTableRow.tsx
+++ src/pages/Andrii/styledComponents/StyledTableRow.tsx
@@ -1,24 +1,15 @@
 import { TableRow } from '@mui/material';
 import { styled } from '@mui/system';
+import { createTransform } from '../animations/animation'
+
+const Transform = createTransform();
 
 export const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  ...Transform,
   '&:nth-of-type(odd)': {
     backgroundColor: theme.palette.secondary.main,
   },
   '&:last-child td, &:last-child th': {
     border: 0,
   },
-  '@keyframes scaleDown': {
-    '0%': {
-      transform: 'scaleY(0)',
-      transformOrigin: 'top',
-      backgroundColor: 'transparent',
-    },
-    '100%': {
-      transform: 'scaleY(1)',
-      transformOrigin: 'top',
-      backgroundColor: theme.palette.background.default,
-    },
-  },
-  animation: 'scaleDown 1s ease-in-out',
 }));
