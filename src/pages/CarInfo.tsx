ccb8d030722a3d1a3fcb9d27b526dd01564f6657
mykyta07 <mykytanyk@gmail.com>
2024-05-24 01:05:04 +0300

    fixed styles

--- src/pages/CarInfo.tsx
+++ src/pages/CarInfo.tsx
@@ -1,5 +1,5 @@
 import CarDetail from "./Mykyta/componets/CarDetail";
-import { Box, Typography } from "@mui/material";
+import { Box, Typography} from "@mui/material";
 import { useParams } from "react-router-dom";
 import { useCarGroupContext } from '../Context';
 import { FormData } from "../interfaces";
@@ -7,7 +7,7 @@ import { useState } from "react";
 
 function CarInfo() {
   const { index } = useParams<{ index: string }>();
-  const carIndex = index ? parseInt(index, 10) : -1; // Use -1 as a fallback index
+  const carIndex = index ? parseInt(index, 10) : -1;
   const { carGroup, updateCar, removeCar } = useCarGroupContext();
   const car = carGroup.cars[carIndex];
   const [isEditing, setIsEditing] = useState(false);
@@ -31,7 +31,7 @@ function CarInfo() {
         variant="h4" 
         sx={{
           textAlign: 'center', 
-          marginTop: {lg: '5%', md: '10%', sm: '15%', xs: '20%'},
+          marginTop: { lg: '5%', md: '10%', sm: '15%', xs: '20%' },
         }}
       >
         Car not found
@@ -42,34 +42,24 @@ function CarInfo() {
   const ContainerStyle = {
     display: "flex",
     flexDirection: "column",
-    justifyContent: "center",
     alignItems: "center",
     marginTop: "2%",
     width: "100%",
     backgroundColor: "background.default",
   };
 
-  const CardLayout = {
-    width: { md: "80%", sm: "100%", xs: "100%" },
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
-    gap: "20%",
-  };
+
 
   return (
     <Box sx={ContainerStyle}>
-      <Box sx={CardLayout}>
-        <CarDetail
-          data={car}
-          onDelete={handleDelete}
-          onSave={handleSave}
-          index={carIndex}
-          onEdit={handleEditClick}
-          isEditing={isEditing}
-        />
-      </Box>
+      <CarDetail
+        data={car}
+        onDelete={handleDelete}
+        onSave={handleSave}
+        index={carIndex}
+        onEdit={handleEditClick}
+        isEditing={isEditing}
+      />
     </Box>
   );
 }
