7e6aa98d16957926f3ff8af261ccfe0c7d6e8dee
Thundem <vovkandrij7@gmail.com>
2024-05-28 16:24:42 +0300

    fixed table row

--- src/pages/Andrii/styledComponents/StyledTableRow.tsx
+++ src/pages/Andrii/styledComponents/StyledTableRow.tsx
@@ -1,11 +1,7 @@
 import { TableRow } from '@mui/material';
 import { styled } from '@mui/system';
-import { createTransform } from '../animations/animation'
-
-const Transform = createTransform();
 
 export const StyledTableRow = styled(TableRow)(({ theme }) => ({
-  ...Transform,
   '&:nth-of-type(odd)': {
     backgroundColor: theme.palette.secondary.main,
   },
