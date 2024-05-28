f670db00f3c61ae82f7ac8e2b6215bb2321605e3
Thundem <vovkandrij7@gmail.com>
2024-05-28 14:16:03 +0300

    added constants to navbar and hamburger menu

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -4,9 +4,13 @@ import { AppBar, Toolbar, Typography, Button, IconButton, Drawer, List, ListItem
 import MenuIcon from '@mui/icons-material/Menu';
 import Brightness7Icon from '@mui/icons-material/Brightness7';
 import Brightness4Icon from '@mui/icons-material/Brightness4';
+import { HOME, ABOUT, CARFORM, ORDERS, CARLIST } from '../constants'
+import { useTheme } from '@mui/material/styles';
+import Logo from '../icon/Logo';
 
 function Layout({ darkMode, toggleDarkMode }: { darkMode: boolean, toggleDarkMode: () => void }) {
     const [drawerOpen, setDrawerOpen] = useState(false);
+    const theme = useTheme();
 
     const handleDrawerToggle = () => {
         setDrawerOpen(!drawerOpen);
@@ -27,7 +31,10 @@ function Layout({ darkMode, toggleDarkMode }: { darkMode: boolean, toggleDarkMod
     };
     
     const BurgerButtons = {
+        display: 'flex',
         color: 'primary.light',
+        justifyContent: 'center',
+        alignItems: 'center',
         borderRadius: '5px',
         padding: '1%',
         width: '100%',
@@ -38,19 +45,19 @@ function Layout({ darkMode, toggleDarkMode }: { darkMode: boolean, toggleDarkMod
     };
     const drawerItems = (
         <List sx={{backgroundColor:"navbar.main", height:"100%",}}>
-            <ListItem sx={{}} component={Link} to="/" onClick={handleDrawerToggle}>
+            <ListItem sx={{}} component={Link} to={HOME} onClick={handleDrawerToggle}>
                 <ListItemText sx={BurgerButtons} primary="Main page" />
             </ListItem>
-            <ListItem component={Link} to="/about-us" onClick={handleDrawerToggle}>
+            <ListItem component={Link} to={ABOUT} onClick={handleDrawerToggle}>
                 <ListItemText sx={BurgerButtons} primary="About Us" />
             </ListItem>
-            <ListItem component={Link} to="/adding-car-form" onClick={handleDrawerToggle}>
+            <ListItem component={Link} to={CARFORM} onClick={handleDrawerToggle}>
                 <ListItemText sx={BurgerButtons} primary="Add Car" />
             </ListItem>
-            <ListItem component={Link} to="/orders" onClick={handleDrawerToggle}>
+            <ListItem component={Link} to={ORDERS} onClick={handleDrawerToggle}>
                 <ListItemText sx={BurgerButtons} primary="Orders" />
             </ListItem>
-            <ListItem component={Link} to="/car-list" onClick={handleDrawerToggle}>
+            <ListItem component={Link} to={CARLIST} onClick={handleDrawerToggle}>
                 <ListItemText sx={BurgerButtons} primary="Car List" />
             </ListItem>
         </List>
@@ -61,14 +68,14 @@ function Layout({ darkMode, toggleDarkMode }: { darkMode: boolean, toggleDarkMod
             <AppBar position="static" sx={NavBar}>
                 <Toolbar>
                     <Typography variant="h6" component="div" sx={{ flexGrow: 1, color: 'primary.light' }}>
-                        Delta
+                        Delta <Logo color={theme.palette.primary.light} width={20} height={20} />
                     </Typography>
                     <Box sx={{ display: {lg: 'flex', md: 'flex', sm: 'flex', xs: 'none'}, justifyContent: 'end', flexGrow: 1 }}>
-                        <Button sx={Buttons} color="inherit" component={Link} to="/">Main page</Button>
-                        <Button sx={Buttons} color="inherit" component={Link} to="/about-us">About Us</Button>
-                        <Button sx={Buttons} color="inherit" component={Link} to="/adding-car-form">Add Car</Button>
-                        <Button sx={Buttons} color="inherit" component={Link} to="/orders">Orders</Button>
-                        <Button sx={Buttons} color="inherit" component={Link} to="/car-list">Car List</Button>
+                        <Button sx={Buttons} color="inherit" component={Link} to={HOME}>Main page</Button>
+                        <Button sx={Buttons} color="inherit" component={Link} to={ABOUT}>About Us</Button>
+                        <Button sx={Buttons} color="inherit" component={Link} to={CARFORM}>Add Car</Button>
+                        <Button sx={Buttons} color="inherit" component={Link} to={ORDERS}>Orders</Button>
+                        <Button sx={Buttons} color="inherit" component={Link} to={CARLIST}>Car List</Button>
                     </Box>
                     <IconButton
                         color="inherit"
@@ -97,7 +104,7 @@ function Layout({ darkMode, toggleDarkMode }: { darkMode: boolean, toggleDarkMod
                         display: { xs: 'block', sm: 'none' }, 
                         color: 'navbar.main',
                         '& .MuiDrawer-paper': {
-                            width: '20%',
+                            minWidth: '35%',
                         },
                     }}
                 >
