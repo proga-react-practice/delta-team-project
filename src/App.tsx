7a3a77e41977e22bc77d6ae0486cdad065681285
Thundem <vovkandrij7@gmail.com>
2024-05-28 14:15:39 +0300

    added constants to App

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ import { theme, darkTheme } from "./theme";
 import { CarGroupProvider } from './Context';
 import { RentCarProvider } from './pages/Andrii/RentCarContext';
 import { useState, useEffect } from 'react';
+import { HOME, ABOUT, CARFORM, ORDERS, CARLIST, CARINFO } from './constants'
 import './App.css';
 import Preloader from './pages/Preloader';
 
@@ -45,13 +46,13 @@ function App() {
             <CssBaseline/>
             <BrowserRouter>
               <Routes>
-                <Route path="/" element={<Layout darkMode={darkMode} toggleDarkMode={toggleDarkMode} />}>
+                <Route path={HOME} element={<Layout darkMode={darkMode} toggleDarkMode={toggleDarkMode} />}>
                   <Route index element={<Home />} />
-                  <Route path="about-us" element={<AboutUs />} />
-                  <Route path="adding-car-form" element={<Car />} />
-                  <Route path="orders" element={<Orders />} />
-                  <Route path="car-list" element={<CarList />} />
-                  <Route path="car/:index" element={<CarInfo />} />
+                  <Route path={ABOUT} element={<AboutUs />} />
+                  <Route path={CARFORM} element={<Car />} />
+                  <Route path={ORDERS} element={<Orders />} />
+                  <Route path={CARLIST} element={<CarList />} />
+                  <Route path={CARINFO} element={<CarInfo />} />
                   <Route path="*" element={<Error />} />
                 </Route>
               </Routes>
