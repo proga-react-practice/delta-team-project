88db290a832b2d6cb83a5244929e6b090593b0f4
Thundem <vovkandrij7@gmail.com>
2024-05-28 14:15:19 +0300

    added route constants

---
+++ src/constants.tsx
@@ -0,0 +1,6 @@
+export const HOME = '/';
+export const ABOUT = '/about-us';
+export const CARFORM = '/adding-car-form';
+export const ORDERS = '/orders';
+export const CARLIST = '/car-list';
+export const CARINFO = '/car/:index';
